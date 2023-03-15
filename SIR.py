8a2995ab84a13c4be167b76e248efdcb8a271f61
wololo <uz21827@bristol.ac.uk>
2023-03-15 13:03:52 +0000

    tttt

--- SIR.py
+++ SIR.py
@@ -82,7 +82,7 @@ needs work to fix as infected doesnt look right, adjust values to make work etc
 '''
 
 # Total population, N.
-N = 6600000
+N = 100000
 # Initial number of infected and recovered individuals, I0 and R0.
 I0, R0 = 1, 0
 # Everyone else, S0, is susceptible to infection initially.
@@ -113,8 +113,8 @@ ax.plot(t, S/1000, 'b', alpha=0.5, lw=2, label='Susceptible')
 ax.plot(t, I/1000, 'r', alpha=0.5, lw=2, label='Infected')
 ax.plot(t, R/1000, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
 ax.set_xlabel('Time /days')
-ax.set_ylabel('Number (1000000s)')
-ax.set_ylim(0,7000)
+ax.set_ylabel('Number (10000s)')
+ax.set_ylim(0,10)
 ax.yaxis.set_tick_params(length=0)
 ax.xaxis.set_tick_params(length=0)
 ax.grid(b=True, which='major', c='w', lw=2, ls='-')

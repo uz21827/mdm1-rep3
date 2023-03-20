c19acb91448dbcc2be9baed1f391d8ecdebc6044
wololo <uz21827@bristol.ac.uk>
2023-03-20 15:56:02 +0000

    Fixed the plots!

--- SIR.py
+++ SIR.py
@@ -9,7 +9,7 @@ Susceptible doesnt work properly so needs fixing
 '''
 # The three parameters of the SIR model
 # Using them as global variables so as not to clutter up the notation
-Npeople = 66000000 # population of the UK
+Npeople = 6600000 # population of the UK
 
 gamma = 1/10 # estimating about 10 days to recovery
 # We'll specify r and deduce beta from beta = gamma*r
@@ -31,9 +31,9 @@ def func_SIR(t,S,I,R):
     return gS,gI,gR
 
 ## Choose a domain of integration
-tmax = 100  # days
+tmax = 365  # days
 ## Choose a time step (try different values)
-dt = 0.02  # i.e.,  a couple of hours
+dt = 1  # i.e.,  a couple of hours
 ## Number of time steps:
 N = int(tmax/dt)
 
@@ -67,31 +67,27 @@ for n in range(0,N-1):
     
 # Plot of the results
 plt.figure(figsize=(8,6))
-plt.semilogy(t,S,'-b',lw=2,label='Susceptible')
-plt.semilogy(t,I,'-r',lw=2,label='Infected')
-plt.semilogy(t,R,'-g',lw=2,label='Recovered')
+plt.plot(t,S,'-b',lw=2,label='Susceptible')
+plt.plot(t,I,'-r',lw=2,label='Infected')
+plt.plot(t,R,'-g',lw=2,label='Recovered')
 plt.xlabel('time (days)')
+plt.xlim(0,tmax)
+plt.ylim(0,Npeople)
 plt.ylabel('Number of people')
-plt.legend()
 plt.title('SIR model for ' + r'$\beta =$' + str(beta)+ r', $\gamma =$' + str(gamma))
 plt.show()
 
 
 
-'''
-odeint solution (more exact), adjust values to fit population etc for covid
-needs work to fix as infected doesnt look right, adjust values to make work etc
-(as this bottom bit is totally copy pasted from google)
-'''
 
 # Total population, N.
-N = 66000000 
+N = 66000000
 # Initial number of infected and recovered individuals, I0 and R0.
 I0, R0 = 1000, 0
 # Everyone else, S0, is susceptible to infection initially.
 S0 = N - I0 - R0
 # Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
-beta, gamma = 0.2, 1/10 
+beta, gamma = 0.2, 1/10
 # A grid of time points (in days)
 t = np.linspace(0, 365, 365)
 
@@ -112,20 +108,13 @@ S, I, R = ret.T
 # Plot the data on three separate curves for S(t), I(t) and R(t)
 
 plt.figure(figsize=(8,6))
-plt.semilogy(t,S,'b',lw=2,label='Susceptible')
-plt.semilogy(t,I,'r',lw=2,label='Infected')
-plt.semilogy(t,R,'g',lw=2,label='Recovered')
+plt.plot(t,S,'b',lw=2,label='Susceptible')
+plt.plot(t,I,'r',lw=2,label='Infected')
+plt.plot(t,R,'g',lw=2,label='Recovered')
 plt.xlabel('time (days)')
 plt.ylabel('Number of people')
-plt.legend()
+plt.xlim(0,365)
+plt.ylim(0,N)
 plt.title('SIR model for ' + r'$\beta =$' + str(beta)+ r', $\gamma =$' + str(gamma))
 plt.show()
-
-
-'''
-Notes:
-Susceptible doesnt seem to work very well, not sure why, Euler method not great
-Look into using odeint from scripy.integrate instead as is more an "exact" result
-Keep both methods though to show progress etc etc
-'''
 

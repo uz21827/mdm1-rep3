6f9f09fe995de9cd467d3b9e5c45213bfab447b9
wololo <uz21827@bristol.ac.uk>
2023-03-24 01:21:35 +0000

    Graphssss

--- SIR.py
+++ SIR.py
@@ -1,76 +1,57 @@
-#MDM1 Rep 3 - SIR 
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 import csv
 
-'''
-Euler Method for solving (numerical method, high inaccuracy),
-Susceptible doesnt work properly so needs fixing
-'''
-
 f = open('UK Covid Data.csv')
 csv_reader = csv.reader(f)
 
-R_values = []
-#create a list of all the current values, ignoring the first line
-for line_no, line in enumerate(csv_reader, 1):
-    if line_no == 1:
-        pass
-    else:
-        R_values.append(line[3])
+# read in R values from CSV file
+R_values_raw = []
+with open('UK Covid Data.csv', 'r') as f:
+    csv_reader = csv.reader(f)
+    next(csv_reader)  # skip header row
+    R_values_raw = [float(row[3]) for row in csv_reader]
 
-print(R_values)
-# The three parameters of the SIR model
-# Using them as global variables so as not to clutter up the notation
-Npeople = 67330000 # population of the UK
+Npeople = 67330000.0 # population of the UK
+gamma = 1/14.0 #14 days to recover
+tmax = 1100  # days
+dt = 1 #time step - 1 day
+N = int(tmax/dt) # umber of time steps
 
-gamma = 1/14 #14 days to recover
 
-#r = 2.39 # R value
+def func_SIR(t, S, I, gamma, R_current):
+    gS_local = - ((gamma * R_current) * S * I) / Npeople
+    gI_local = (gamma * R_current * S * I / Npeople) - gamma * I
+    gR_local = gamma * I
+    return gS_local, gI_local, gR_local
 
-#beta =  gamma*r
-
-## Computes all the right-hand side functions
-def func_SIR(t,S,I,R,gamma,r):
-    gS = - ((gamma*r[n+1])*S*I)/Npeople
-    gI = (gamma*r[n+1]*S*I/Npeople) - gamma*I
-    gR = gamma*I
-    # We return the g functions
-    return gS,gI,gR
-
-## Choose a domain of integration
-tmax = 365  # days
-## Choose a time step (try different values)
-dt = 1
-## Number of time steps:
-N = int(tmax/dt)
-
-## Allocate the arrays
-t = np.zeros((N,))
+# Allocate the arrays
+t = np.linspace(0, tmax, N)
 S = np.zeros((N,))
 I = np.zeros((N,))
 R = np.zeros((N,))
 
-## Initial condition 1: A handful of infected
-I[0] = 24
+# Initial condition 1: A handful of infected
+I[0] = 24.0
 
-## and nobody has recovered and is therefore immune
-R[0] = 68
+# and nobody has recovered and is therefore immune
+R[0] = 68.0
 
-## Everybody else is healthy but susceptible
+# Everybody else is healthy but susceptible
 S[0] = Npeople - R[0] - I[0]
 
-
-## Implementing the Euler algorithm
-for n in range(0,N-1):
+# Implementing the Euler algorithm
+for n in range(0, N-1):
     # Compute all three right hand side functions at t[n] 
-    gS,gI,gR = func_SIR(t[n],S[n],I[n],R[n],gamma,R_values[n])
+    gS, gI, gR = func_SIR(t[n], S[n], I[n], gamma, R_values_raw[n])
     # Update time, S, I and R according to the Euler rule
+    S[n+1] = S[n] + gS * dt
+    I[n+1] = I[n] + gI * dt
+    R[n+1] = R[n] + gR * dt
     t[n+1] = t[n] + dt
-    S[n+1] = S[n] + gS*dt
-    I[n+1] = I[n] + gI*dt
-    R[n+1] = R[n] + gR*dt
+    print("R: ", R_values_raw[n],"    Infected:", I[n],"    Day:", t[n])
+   
     
 # Plot of the results
 plt.figure(figsize=(8,6))
@@ -81,5 +62,5 @@ plt.xlabel('time (days)')
 plt.xlim(0,tmax)
 plt.ylim(0,Npeople)
 plt.ylabel('Number of people (millions)')
-plt.title('SIR model for ' + r'$\beta =$' + str(beta)+ r', $\gamma =$' + str(gamma))
+plt.title('SIR model for UK Covid Data - R value updated each time step')
 plt.show()
